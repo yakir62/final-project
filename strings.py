b7b8c6e709fb56063dc07441539b2bf71c7b6bac
yakir <yakirhd62@gmail.com>
2020-01-26 17:39:01 +0200

    Final Project 1.1

--- strings.py
+++ strings.py
@@ -7,41 +7,45 @@ Create a function that returns the words in reverse order:
 Example: ['circus', 'flying', 'pythons', 'monty']
 Create a function that returns a list of 4 character strings:
 Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
-
 """
-import pytest
-
+# import pytest
+from collections import OrderedDict
 def no_duplicates(a_string):
-    pass
-
-
+    return  ''.join(sorted(set(a_string)));
+print("***sorted and no duplicates:***")
+print(no_duplicates("monty pythons flying circus"));
+print("\n")
 def reversed_words(a_string):
-    pass
-
-
+    l=list(a_string.split(" "))
+    newl=[]
+    for i in reversed(l):
+        newl.append(i);
+    return newl;
+print("***in reverse:***");
+print(reversed_words("monty pythons flying circus"))
+print("\n")
 def four_char_strings(a_string):
-    pass
-
-
-def test_no_duplicates():
-    s = 'monty pythons flying circus'
-    assert no_duplicates(s) == ' cfghilmnoprstuy'
-
-
-def test_reversed_words():
-    s = 'monty pythons flying circus'
-    assert reversed_words(s) == ['circus', 'flying', 'pythons', 'monty']
-
-
-def test_four_char_strings():
-    s = 'monty pythons flying circus'
-    assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
-
-
-def main():
-    return pytest.main(__file__)
-
-
-if __name__ == '__main__':
-    main()
-    
+    l=[a_string[i:i+4] for i in range(0, len(a_string), 4)]
+    return l
+print("***string sliced by four***")
+print(four_char_strings("monty pythons flying circus"));
+# def test_no_duplicates():
+#     s = 'monty pythons flying circus'
+#     assert no_duplicates(s) == ' cfghilmnoprstuy'
+#
+#
+# def test_reversed_words():
+#     s = 'monty pythons flying circus'
+#     assert reversed_words(s) == ['circus', 'flying', 'pythons', 'monty']
+#
+#
+# def test_four_char_strings():
+#     s = 'monty pythons flying circus'
+#     assert four_char_strings(s) == ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
+#
+# def main():
+#     return pytest.main(__file__)
+#
+#
+# if __name__ == '__main__':
+#     main()
